129be603b7f628e721c67a823133beef754aa5e6
Martin Tomasella <martin.tomasella@inteltech-solutions.com>
2026-05-02 17:32:26 -0300

    fix: plugin withKotlinVersion fuerza kotlin 1.9.25 en gradle

---
+++ plugins/withKotlinVersion.js
@@ -0,0 +1,17 @@
+﻿const { withProjectBuildGradle } = require("@expo/config-plugins");
+
+module.exports = function withKotlinVersion(config) {
+  return withProjectBuildGradle(config, (config) => {
+    config.modResults.contents = config.modResults.contents.replace(
+      /kotlinVersion\s*=\s*["'].*["']/,
+      'kotlinVersion = "1.9.25"'
+    );
+    if (!config.modResults.contents.includes('kotlinVersion = "1.9.25"')) {
+      config.modResults.contents = config.modResults.contents.replace(
+        "ext {",
+        'ext {\n        kotlinVersion = "1.9.25"'
+      );
+    }
+    return config;
+  });
+};
